c997979e5f8a9c76163148e372be11930d3a6bc5
sakibmd <sakibmd.cse@gmail.com>
2020-04-20 01:24:52 +0600

    project-1 is completed

--- simple-project-1/app.js
+++ simple-project-1/app.js
@@ -3,5 +3,95 @@ new Vue({
     data: {
         playerHealth: 100,
         monstarHealth: 100,
+        gameIsRunning: false,
+        turns: [],
+    },
+
+    methods: {
+        startGame: function(){
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monstarHealth = 100;
+            this.turns = [];
+        },
+
+        attack: function(){
+            var damage = this.calculateDamage(3,10);
+            this.monstarHealth  -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage,
+            });
+            if(this.checkWin()){
+                return;
+            }
+            this.monstarAttack();
+
+        },
+
+        specialAttack: function(){
+            damage = this.calculateDamage(10,20);
+            this.monstarHealth  -= damage;
+            if(this.checkWin()){
+                return;
+            }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits hard Monster for ' + damage,
+            });
+            this.monstarAttack();
+        },
+
+        heal: function(){
+            if(this.playerHealth <= 90){
+                this.playerHealth += 10;
+            }else{
+                this.playerHealth = 100;
+            }
+
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for 10',
+            });
+            this.monstarAttack();
+        },
+
+        giveUp: function(){
+            this.gameIsRunning = false;
+        },
+
+        monstarAttack: function(){
+            damage = this.calculateDamage(5,12);
+            this.playerHealth  -= damage;
+            this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage,
+            });
+        },
+
+        calculateDamage: function(min,max){
+            return Math.max(Math.floor(Math.random() * 10) + 1, min);
+        },
+
+        checkWin: function(){
+            if(this.monstarHealth <= 0){
+                if(confirm('You Won! New Game?')){
+                    this.startGame();
+                }else{
+                    this.gameIsRunning = false;
+                }
+                return true;
+                }
+            else if(this.playerHealth <= 0){
+                if(confirm('You Lost! New Game?')){
+                    this.startGame();
+                }else{
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+        }
     }
 });
