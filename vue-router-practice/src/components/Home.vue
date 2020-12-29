ad2971616842b924d6c7271659b89010d68d6727
sakibmd <sakibmd.cse@gmail.com>
2020-12-29 21:27:31 +0600

    practicing vue-router latest

---
+++ vue-router-practice/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <p>This is Home Page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ vue-router-practice/src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/components/Home'
+import About from '@/components/About'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   name: 'About',
+  //   //component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
