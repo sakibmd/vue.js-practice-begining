0651e6f78e7dae8ee0493548cee2ded4b2539a49
Sakib Mohammed <sakibmd.cse@gmail.com>
2021-06-11 01:42:45 +0600

    practicing vue3 component handling with some advance features

---
+++ vue3-cmp-communication-problem-solution-using-provide-inject/src/components/KnowledgeElement.vue
@@ -0,0 +1,15 @@
+<template>
+  <li>
+    <h3>{{ topicName }}</h3>
+    <p>{{ description }}</p>
+    <p>Count Value is: {{ counterValue.value }}</p>
+    <button @click="selectTopic(id)">Learn More</button>
+  </li>
+</template>
+
+<script>
+export default {
+  inject: ['selectTopic', 'counterValue'],
+  props: ['id', 'topicName', 'description'],
+};
+</script>
---
+++ vue3-cmp-communication-problem-solution-using-provide-inject/src/components/KnowledgeGrid.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <knowledge-element
+      v-for="topic in allTopics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+    ></knowledge-element>
+  </ul>
+</template>
+
+<script>
+export default {
+  inject: ['allTopics'],
+};
+</script>
