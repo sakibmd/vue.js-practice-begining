199d9ccabe61b690dffd840de555709497b975e9
Sakib Mohammed <sakibmd.cse@gmail.com>
2021-06-13 23:57:24 +0600

    learn vu3e form handling

---
+++ vue3-forms-handling/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+ <!-- important resource:
+ How to use v-model in custom component: https://stackoverflow.com/questions/46258763/vuejs-v-model-in-custom-component  -->
+
+  <the-form></the-form>
+</template>
+
+<script>
+import TheForm from './components/TheForm.vue';
+
+export default {
+  components: {
+    TheForm
+  }  
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+  background-color: #292929;
+}
+</style>
---
+++ vue3-forms-handling/src/components/YearControl.vue
@@ -0,0 +1,15 @@
+<template>
+    <!-- When we use input field then we can apply that type of syntex.
+    modelValue & update:modelValue is by default: https://stackoverflow.com/questions/46258763/vuejs-v-model-in-custom-component
+    we accect this in another page like: <r-c v-model="rating"></rx> & data=> rating:null then console somewhere -->
+    <label>Current Year</label>
+    <input type="number" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" >
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+};
+</script>
+
